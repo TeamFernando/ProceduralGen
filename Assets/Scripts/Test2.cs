39248428831648c885cb8aa20b09391502b9a6d2
Jakob Wax <waxjakob@gmail.com>
2024-10-30 12:59:10 -0400

    Add assets

---
+++ Assets/Scripts/Test2.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+
+public class Test2 : MonoBehaviour
+{
+
+    public void Awake()
+    {
+        int 3.0f = "g";
+    }
+
+    public void Start()
+    {
+        
+    }
+}
